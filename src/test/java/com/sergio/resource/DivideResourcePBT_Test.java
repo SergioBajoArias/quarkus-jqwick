75082ade96f705c418862b08a97e529a1d996ca4
Sergio Bajo <sergio.bajo@xeridia.com>
2024-10-07 12:58:42 +0200

    Testing with JQwick

--- src/test/java/com/sergio/resource/DivideResourcePBT_Test.java
+++ src/test/java/com/sergio/resource/DivideResourcePBT_Test.java
@@ -1,47 +1,25 @@
 package com.sergio.resource;
 
-import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
-import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
+import static io.restassured.RestAssured.given;
 import static org.quicktheories.QuickTheory.qt;
 import static org.quicktheories.generators.SourceDSL.integers;
 
 @QuarkusTest
 class DivideResourcePBT_Test {
 
-    @TestHTTPResource("/")
-    URL baseURL;
-
     @Test
     void testDivide() {
         qt()
             .forAll(integers().all(), integers().all().assuming(i -> i != 0))
             .checkAssert((dividend, divisor) -> {
-                try (HttpClient client = HttpClient.newBuilder().build()) {
-
-                    HttpRequest request = HttpRequest.newBuilder()
-                            .uri(new URI(baseURL.toString() + "/divide/" + dividend + "/" + divisor))
-                            .GET()
-                            .build();
-
-                    HttpResponse<Void> response = client.send(request, HttpResponse.BodyHandlers.discarding());
-
-                    Assertions.assertEquals(HttpStatus.SC_OK, response.statusCode());
-                } catch (URISyntaxException | InterruptedException | IOException e) {
-                    throw new RuntimeException(e);
-                }
+                given()
+                    .when()
+                        .get("/divide/" + dividend + "/" + divisor)
+                    .then()
+                        .statusCode(200);
             });
     }
 }
